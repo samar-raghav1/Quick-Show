740d22d79124fd86216a522c1019928e1a7a8b62
samar-raghav1 <samarraghav001@gmail.com>
2025-07-05 21:44:03 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+
+import  Home  from "./pages/Home"
+import React from 'react'
+import { Routes,Route, useLocation } from 'react-router-dom'
+import Movies from "./pages/Movies"
+import MovieDetails from "./pages/MovieDetails"
+import Seatlayout from "./pages/Seatlayout"
+import MyBooking from "./pages/MyBooking"
+import Favorite from "./pages/Favorite"
+import Navbar from "./components/Navbar"
+import { Toaster } from "react-hot-toast"
+import Footer from "./components/Footer"
+
+const App = () => {
+  const isAdminRoute=useLocation().pathname.startsWith('/admin')
+  return (
+    <>
+    <Toaster/>
+    {isAdminRoute&&  <Navbar/>}
+     
+      <Routes>
+       <Route path="/" element={<Home/>} />
+       <Route path="/movies" element={<Movies/>} />
+       <Route path="/movies/:id" element={<MovieDetails/>} />
+       <Route path="/movies/:id/date" element={<Seatlayout/>} />
+       <Route path="/mybooking" element={<MyBooking/>} />
+       <Route path="/favorite" element={<Favorite/>} />
+      </Routes>
+       {isAdminRoute&&  <Footer/>}
+    </>
+  )
+}
+
+export default App
---
+++ client/src/pages/Seatlayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Seatlayout = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Seatlayout
