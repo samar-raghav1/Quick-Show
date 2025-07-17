ddd9659e17ebe9345d9fd3e59100ffa49551af10
samar-raghav1 <samarraghav001@gmail.com>
2025-07-17 23:02:48 +0530

    new

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -15,7 +15,7 @@ const syncUserCreation=inngest.createFunction(
             name:first_name +' '+last_name,
             image:image_url 
         }
-        await userData.create(userData)
+        await User.create(userData)
 
     }
 )
