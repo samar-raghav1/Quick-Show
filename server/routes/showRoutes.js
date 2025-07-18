7d04faa1f8b701dfb18eab6391e644d24be5c2ff
samar-raghav1 <samarraghav001@gmail.com>
2025-07-18 11:52:29 +0530

    frontend

--- server/routes/showRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getNowPlayingMovies } from "../controllers/showController.js";
-
-const showRouter=express.Router();
-showRouter.get('/now-playing',getNowPlayingMovies)
-
-
-export default showRouter;
