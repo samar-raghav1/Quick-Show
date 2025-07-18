d7c3a427d84604686dc9e1a92320709055db550c
samar-raghav1 <samarraghav001@gmail.com>
2025-07-18 09:42:18 +0530

    server

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
+import showRouter from "./routes/showRoutes.js";
 const app= express();
 const port=3000;
 await connectDB();
@@ -15,6 +16,7 @@ app.use(clerkMiddleware())
 //Api Routes
 app.get('/',(req,res)=>res.send("Server is live.."));
 app.use('/api/inngest',serve({client:inngest,functions}))
+app.use('/api/show',showRouter)
 
 
 app.listen(port,()=>console.log(`listing at http://localhost:${port}`));
