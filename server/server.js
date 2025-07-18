a28a4c83ec32b76add7daa5be559f705f7df07e2
samar-raghav1 <samarraghav001@gmail.com>
2025-07-18 12:15:43 +0530

    server

--- server/server.js
+++ server/server.js
@@ -2,15 +2,22 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/db.js";
-
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
+import showRouter from "./routes/showRoutes.js";
 
 const app= express();
 const port=3000;
 await connectDB();
 
-
+app.use(express.json())
+app.use(cors())
+app.use(clerkMiddleware())
+//Api routes
 app.get('/',(req,res)=>res.send("Server is live.."));
-
+app.use('/api/inngest',serve({client:inngest,functions}))
+app.use('/api/show',showRouter)
 
 
 
